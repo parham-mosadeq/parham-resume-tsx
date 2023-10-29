c82379ba5b3d16e20f581067ce66d6964c1d010f
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-10-29 21:47:18 +0330

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { ComponentsChildren } from '@/types/types';
 import Layout from '@/components/Layout/Layout';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
+});
+
 export const metadata: Metadata = {
   title: 'Parham',
   description: "Parham mosadeq's website,resume",
@@ -10,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: ComponentsChildren) {
   return (
     <html lang='en'>
-      <body>
+      <body className={roboto.className}>
         <Layout>{children}</Layout>
       </body>
     </html>
