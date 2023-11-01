52d21dde6862f2581ada1815f1fb94fd20cbb293
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-11-01 23:34:16 +0330

    finish about me

--- src/components/templates/AboutPage.tsx
+++ src/components/templates/AboutPage.tsx
@@ -1,6 +1,60 @@
 import Image from 'next/image';
 import React from 'react';
 import styles from '#/src/styles/about.module.css';
+import {
+  SiTypescript,
+  SiJavascript,
+  SiReact,
+  SiGit,
+  SiAdobexd,
+  SiAdobeillustrator,
+  SiAdobephotoshop
+} from 'react-icons/si';
+import { AiFillHtml5 } from 'react-icons/ai';
+import { BiLogoCss3 } from 'react-icons/bi';
+
+const techStack = [
+  {
+    id: 1,
+    val: <SiTypescript />,
+  },
+  {
+    id: 2,
+    val: <SiJavascript />,
+  },
+  {
+    id: 3,
+    val: <SiReact />,
+  },
+  {
+    id: 4,
+    val: <SiGit />,
+  },
+  {
+    id: 5,
+    val: <AiFillHtml5 />,
+  },
+  {
+    id: 6,
+    val: <BiLogoCss3 />,
+  },
+];
+
+const designs = [
+  {
+    id: 7,
+    val: <SiAdobexd />,
+  },
+  {
+    id: 8,
+    val: <SiAdobeillustrator />,
+  },
+  {
+    id: 9,
+    val: <SiAdobephotoshop />,
+  },
+];
+
 export default function AboutPage() {
   return (
     <div className={styles.container}>
@@ -53,33 +107,42 @@ export default function AboutPage() {
         {/* designs */}
         <div className={styles.tech_designs}>
           <ul className={styles.tech_designs_items}>
-            <li>
-              <p>adobe xd</p>
+            {/* <li>
+              <SiAdobexd />
             </li>
             <li>
-              <p>adobe illustrator</p>
-            </li>
+              <SiAdobeillustrator />
+            </li> */}
+            {designs.map((i) => {
+              return <li key={i.id}>{i.val}</li>;
+            })}
           </ul>
         </div>
         {/* designs */}
         {/* codes */}
         <div className={styles.codes}>
           <ul>
-            <li>
-              <p>react</p>
+            {/* <li>
+              <AiFillHtml5 />
             </li>
             <li>
-              <p>html</p>
+              <BiLogoCss3 />
             </li>
             <li>
-              <p>css</p>
+              <SiJavascript />
             </li>
             <li>
-              <p>css</p>
+              <SiReact />
             </li>
             <li>
-              <p>css</p>
+              <SiTypescript />
             </li>
+            <li>
+              <SiGit />
+            </li> */}
+            {techStack.map((i) => {
+              return <li key={i.id}>{i.val}</li>;
+            })}
           </ul>
         </div>
         {/* codes */}
