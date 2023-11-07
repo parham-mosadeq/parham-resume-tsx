38dfab6250e5823076ec1068a0dc9a3faeb79619
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-11-07 23:24:32 +0330

    create work page

--- src/components/templates/WorkPage.tsx
+++ src/components/templates/WorkPage.tsx
@@ -1,7 +1,183 @@
-import React from 'react'
-
+import Image from 'next/image';
+import styles from '#/src/styles/work.module.css';
 export default function WorkPage() {
   return (
-    <div>WorkPage</div>
-  )
+    <div className={styles.container}>
+      {/* card start */}
+      <div className={styles.cardContainer}>
+        {/* img */}
+        <div className={styles.imgContainer}>
+          <Image src='/images/code-logo.jfif' width={750} height={750} alt='' />
+        </div>
+        {/* img */}
+
+        {/* work desc */}
+        <div className={styles.descContainer}>
+          <div className={styles.descriptions}>
+            {/* title */}
+            <h4>
+              <a href='/'>shop</a>
+            </h4>
+            <span>1402/05/24</span>
+            {/* techs */}
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+              quis neque, aliquam eos quam dolore facilis sint cum nesciunt
+              quas.
+            </p>
+          </div>
+          <div className={styles.techContainer}>
+            <ul>
+              <li>react</li>
+              <li>tailwind</li>
+              <li>next - 13</li>
+              <li>axios</li>
+            </ul>
+          </div>
+        </div>
+        {/* work desc */}
+      </div>
+      {/* card start */}
+      {/* card start */}
+      <div className={styles.cardContainer}>
+        {/* img */}
+        <div className={styles.imgContainer}>
+          <Image src='/images/code-logo.jfif' width={750} height={750} alt='' />
+        </div>
+        {/* img */}
+
+        {/* work desc */}
+        <div className={styles.descContainer}>
+          <div className={styles.descriptions}>
+            {/* title */}
+            <h4>
+              <a href='/'>shop</a>
+            </h4>
+            <span>1402/05/24</span>
+            {/* techs */}
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+              quis neque, aliquam eos quam dolore facilis sint cum nesciunt
+              quas.
+            </p>
+          </div>
+          <div className={styles.techContainer}>
+            <ul>
+              <li>react</li>
+              <li>tailwind</li>
+              <li>next - 13</li>
+              <li>axios</li>
+            </ul>
+          </div>
+        </div>
+        {/* work desc */}
+      </div>
+      {/* card start */}
+      {/* card start */}
+      <div className={styles.cardContainer}>
+        {/* img */}
+        <div className={styles.imgContainer}>
+          <Image src='/images/code-logo.jfif' width={750} height={750} alt='' />
+        </div>
+        {/* img */}
+
+        {/* work desc */}
+        <div className={styles.descContainer}>
+          <div className={styles.descriptions}>
+            {/* title */}
+            <h4>
+              <a href='/'>shop</a>
+            </h4>
+            <span>1402/05/24</span>
+            {/* techs */}
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+              quis neque, aliquam eos quam dolore facilis sint cum nesciunt
+              quas.
+            </p>
+          </div>
+          <div className={styles.techContainer}>
+            <ul>
+              <li>react</li>
+              <li>tailwind</li>
+              <li>next - 13</li>
+              <li>axios</li>
+            </ul>
+          </div>
+        </div>
+        {/* work desc */}
+      </div>
+      {/* card start */}
+      {/* card start */}
+      <div className={styles.cardContainer}>
+        {/* img */}
+        <div className={styles.imgContainer}>
+          <Image src='/images/code-logo.jfif' width={750} height={750} alt='' />
+        </div>
+        {/* img */}
+
+        {/* work desc */}
+        <div className={styles.descContainer}>
+          <div className={styles.descriptions}>
+            {/* title */}
+            <h4>
+              <a href='/'>shop</a>
+            </h4>
+            <span>1402/05/24</span>
+            {/* techs */}
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+              quis neque, aliquam eos quam dolore facilis sint cum nesciunt
+              quas.
+            </p>
+          </div>
+          <div className={styles.techContainer}>
+            <ul>
+              <li>react</li>
+              <li>tailwind</li>
+              <li>next - 13</li>
+              <li>axios</li>
+            </ul>
+          </div>
+        </div>
+        {/* work desc */}
+      </div>
+      {/* card start */}
+      {/* card start */}
+      <div className={styles.cardContainer}>
+        {/* img */}
+        <div className={styles.imgContainer}>
+          <Image src='/images/code-logo.jfif' width={750} height={750} alt='' />
+        </div>
+        {/* img */}
+
+        {/* work desc */}
+        <div className={styles.descContainer}>
+          <div className={styles.descriptions}>
+            {/* title */}
+            <h4>
+              <a href='/'>shop</a>
+            </h4>
+            <span>1402/05/24</span>
+            {/* techs */}
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+              quis neque, aliquam eos quam dolore facilis sint cum nesciunt
+              quas.
+            </p>
+          </div>
+          <div className={styles.techContainer}>
+            <ul>
+              <li>react</li>
+              <li>tailwind</li>
+              <li>next - 13</li>
+              <li>axios</li>
+            </ul>
+          </div>
+        </div>
+        {/* work desc */}
+      </div>
+      {/* card start */}
+    </div>
+  );
 }
